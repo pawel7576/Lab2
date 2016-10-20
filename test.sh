c650445a1796f359b0498d77d494e3d7aa132887
Pawel Jasinski <pawel.jasinski@ihs.com>
2016-10-20 23:32:06 +0200

    new tests

--- test.sh
+++ test.sh
@@ -5,6 +5,13 @@ echo "first tests"
 for i in {1..5}
 do
   echo $i
-  cat firstTests/test$i | ./a.out 100 | diff firstTests/wzor$i -
+  cat tests/firstTests/test$i | ./a.out 100 | diff tests/firstTests/wzor$i -
+
+done
+
+for i in {1..10}
+do
+echo $i
+cat tests/testyFB/d$i | ./a.out 16 | diff tests/testyFB/W$i -
 
 done
